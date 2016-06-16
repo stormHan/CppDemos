390e1e1b2eed8a7f6486eb566501f1d185db61b2
stormHan <stormHan1205@gmail.com>
2016-06-16 17:57:09 +0800

    use Process class to Control

---
+++ Coursera/Warcraft/Process.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <queue>
+
+#include "Headquarter.h"
+
+class Process
+{
+public:
+	Process(Headquarter *_pHead) : pHead(_pHead){};
+
+
+private:
+	Headquarter *pHead;
+	std::queue<int> m_queue;
+};
